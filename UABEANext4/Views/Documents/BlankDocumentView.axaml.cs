cd50023e5896ea0154cba16c3918165bbf4218d3
nesquack <karsen.beck@gmail.com>
2025-09-15 23:05:39 -0400

    add blank document view

---
+++ UABEANext4/Views/Documents/BlankDocumentView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace UABEANext4.Views.Documents;
+public partial class BlankDocumentView : UserControl
+{
+    public BlankDocumentView()
+    {
+        InitializeComponent();
+    }
+}
