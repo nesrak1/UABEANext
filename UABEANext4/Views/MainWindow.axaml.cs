75e4e705efdde1d79277d905291ef7543c87bb0b
nesquack <karsen.beck@gmail.com>
2025-12-02 23:58:36 -0500

    reenable adblock

--- UABEANext4/Views/MainWindow.axaml.cs
+++ UABEANext4/Views/MainWindow.axaml.cs
@@ -4,6 +4,10 @@ using Avalonia.Platform.Storage;
 using System.Linq;
 using System.Threading.Tasks;
 using UABEANext4.ViewModels;
+#if DEBUG
+using Avalonia.Diagnostics;
+using UABEANext4.Logic.DevTools;
+#endif
 
 namespace UABEANext4.Views;
 
@@ -12,8 +16,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
 #if DEBUG
-        InitializeComponent(attachDevTools: true);
-        //DevToolsAdblock.Attach(this, new DevToolsOptions());
+        InitializeComponent(attachDevTools: false);
+        DevToolsAdblock.Attach(this, new DevToolsOptions());
 #else
         InitializeComponent();
 #endif
