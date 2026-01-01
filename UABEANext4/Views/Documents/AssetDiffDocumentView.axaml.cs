5208330dc6ee8041cb6829074a05f8db58cdd1f5
Alex(GoD) <51307259+darthalex2014@users.noreply.github.com>
2026-01-01 23:22:22 +0500

    Create AssetDiffDocumentView.axaml.cs

---
+++ UABEANext4/Views/Documents/AssetDiffDocumentView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace UABEANext4.Views.Documents;
+public partial class AssetDiffDocumentView : UserControl
+{
+    public AssetDiffDocumentView()
+    {
+        InitializeComponent();
+    }
+}
