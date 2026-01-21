74ecd7e2f0f3d9c3f0b0f6c1df4caf5b07d2f714
MrIkso <mrikso821@gmail.com>
2026-01-21 20:26:45 +0200

    fix: use WindowUtils.GetMainWindow() for access clipboard

--- UABEANext4/Util/ApplicationExtensions.cs
+++ UABEANext4/Util/ApplicationExtensions.cs
@@ -1,40 +1,15 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.VisualTree;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace UABEANext4.Util;
 public static class ApplicationExtensions
 {
-    /// <summary>
-    /// Returns the TopLevel from the main window or view. 
-    /// </summary>
-    /// <param name="app">The application to get the TopLevel for.</param>
-    /// <returns>A TopLevel object.</returns>
-    public static TopLevel? GetTopLevel(this Application? app)
-    {
-        if (app?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            return desktop.MainWindow;
-        }
-        if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView })
-        {
-            var visualRoot = mainView.GetVisualRoot();
-            if (visualRoot is TopLevel topLevel)
-            {
-                return topLevel;
-            }
-        }
-        return null;
-    }
 
     public static async Task CopyToClipboard(string text)
     {
-        var topLevel = Application.Current.GetTopLevel();
-        if (topLevel?.Clipboard != null)
+        var mainWindow = WindowUtils.GetMainWindow();
+        if (mainWindow?.Clipboard != null)
         {
-            await topLevel.Clipboard.SetTextAsync(text);
+            await mainWindow.Clipboard.SetTextAsync(text);
         }
     }
 
